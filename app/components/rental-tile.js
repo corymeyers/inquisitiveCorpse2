b85295504379926a1efbff0ed9b53e46a4e7c0d4
cory <whippoorwill.in.flight@gmail.com>
2016-06-17 15:21:49 -0700

    add broken service

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,9 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   updateRentalForm: false,
-
+  shoppingCart: Ember.inject.service(),
   fullLocation: Ember.computed('rental.owner', 'rental.type', function() {
     return this.get('rental.type') +  "-"  +'Answered by ' + this.get('rental.owner');
-  })
+  }),
 
+  actions: {
+   addToCart(item) {
+     this.get('shoppingCart').add(item);
+   },
+}
 });
